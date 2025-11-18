b2b9f423f45373c3321293361b78129abb4c160b
u202314539 <u202314539@upc.edu.pe>
2025-11-17 22:47:48 -0500

    Prediction Terminado

--- Neurokid/src/app/components/predicciones/predicciones-insert/predicciones-insert.ts
+++ Neurokid/src/app/components/predicciones/predicciones-insert/predicciones-insert.ts
@@ -1,11 +1,96 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { Prediction } from '../../../models/Prediction';
+import { User } from '../../../models/User';
+import { Predictionservice } from '../../../services/predictionservice';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Userservice } from '../../../services/userservice';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @Component({
   selector: 'app-predicciones-insert',
-  imports: [],
+  imports: [ ReactiveFormsModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatSelectModule, CommonModule, MatRadioModule, MatButtonModule ],
   templateUrl: './predicciones-insert.html',
   styleUrl: './predicciones-insert.css',
 })
-export class PrediccionesInsert {
+export class PrediccionesInsert implements OnInit {
+  form: FormGroup = new FormGroup({});
+  pre: Prediction = new Prediction();
+  edicion: boolean = false;
 
+  listaUsuario: User[] = [];
+
+  id:number = 0;
+
+  constructor(private pS: Predictionservice,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private uS: Userservice
+  ) {}
+
+  ngOnInit(): void {
+      this.route.params.subscribe((data: Params) => {
+        this.id = data['id'];
+        this.edicion = data['id'] != null;
+        this.init();
+      });
+
+      this.uS.list().subscribe((data) => {
+        this.listaUsuario = data;
+      });
+
+      this.form = this.formBuilder.group({
+        id: [''],
+        puntaje: ['', Validators.required],
+        texto: ['', Validators.required],
+        prediccion: ['', Validators.required],
+        usuario: ['', Validators.required]
+      });
+  }
+
+  aceptar(): void {
+    if (this.form.valid) {
+      this.pre.predictionId = this.form.value.id;
+      this.pre.predictionScore = this.form.value.puntaje;
+      this.pre.explanationText = this.form.value.texto;
+      this.pre.predictedAt = this.form.value.prediccion;
+      this.pre.usuario.username = this.form.value.usuario;
+
+      if(this.edicion) {
+        this.pS.update(this.pre).subscribe(() => {
+          this.pS.list().subscribe((data) => {
+            this.pS.setList(data);
+          });
+        });
+      } else {
+        this.pS.insert(this.pre).subscribe((data) => {
+          this.pS.list().subscribe((data) => {
+            this.pS.setList(data);
+          });
+        });
+      }
+      this.router.navigate(['Prediction']);
+    }
+  }
+
+  init() {
+    if (this.edicion) {
+      this.pS.listId(this.id).subscribe((data) => {
+        this.form = new FormGroup({
+          id: new FormControl(data.predictionId),
+          puntaje: new FormControl(data.predictionScore),
+          texto: new FormControl(data.explanationText),
+          prediccion: new FormControl(data.predictedAt),
+          usuario: new FormControl(data.usuario.username)
+        });
+      });
+    }
+  }
 }
--- Neurokid/src/app/components/predicciones/predicciones.ts
+++ Neurokid/src/app/components/predicciones/predicciones.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { PrediccionesListar } from './predicciones-listar/predicciones-listar';
 
 @Component({
   selector: 'app-predicciones',
-  imports: [],
+  imports: [ RouterOutlet, PrediccionesListar ],
   templateUrl: './predicciones.html',
   styleUrl: './predicciones.css',
 })
 export class Predicciones {
-
+  constructor(public route:ActivatedRoute) {}
 }

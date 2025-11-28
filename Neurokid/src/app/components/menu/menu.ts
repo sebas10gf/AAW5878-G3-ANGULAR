225d701b5826252f96b355e858c28ff0b8f5119a
Sebastian Gonzales Flores <sebastiangonzales0601@gmail.com>
2025-11-28 18:21:11 -0500

    usuario update

--- Neurokid/src/app/components/menu/menu.ts
+++ Neurokid/src/app/components/menu/menu.ts
@@ -15,8 +15,7 @@ import { loginservice } from '../../services/loginservice';
 export class Menu {
   role: string = '';
   usuario: string = '';
-
-  constructor(private loginService: loginservice) {}
+  constructor(private loginService: loginservice,) {}
 
   cerrar() {
     sessionStorage.clear();
@@ -24,8 +23,10 @@ export class Menu {
 
   verificar() {
     this.role = this.loginService.showRole();
+    this.usuario = this.loginService.showName();
     return this.loginService.verificar();
   }
+
 
   isAdmin() {
     return this.role === 'ADMIN';

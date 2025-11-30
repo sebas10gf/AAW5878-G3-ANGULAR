3d2b3af349186dfad17b3bf3b75d8b083f97dab6
Sebastian Gonzales Flores <sebastiangonzales0601@gmail.com>
2025-11-30 17:38:26 -0500

    as

--- Neurokid/src/app/app.ts
+++ Neurokid/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Menu } from "./components/menu/menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Menu],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

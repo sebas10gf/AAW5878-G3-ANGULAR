0d13fba5bd5f6f9c4c5ad4f8703057d79cf6ec57
Sebastian Gonzales Flores <sebastiangonzales0601@gmail.com>
2025-11-30 22:34:06 -0500

    as

--- Neurokid/src/app/services/geminiservice.ts
+++ Neurokid/src/app/services/geminiservice.ts
@@ -25,6 +25,7 @@ export class Geminiservice {
               "Siempre respondes en un lenguaje claro y humano."+
               "Cualquier consulta o texto enviado sobre temas no relacionados a lo psicoligico debes responder No entiendo tu mensaje podrias enviarlo denuevo porfavor?." +
               "Si te llega alguna peticion que inicie de la forma Realiza una prediccion sobre el usuario... Debes realizar la prediccion de cual crees que sea la causa de su problema de manera con concisa y corta y asignale una puntuacion del 1 al 10 donde 1 es leve y 10 es grave y no agregues el inicio del promt en tu respuesta." + 
+              "Si el contexto que se te de en la peticion sobre la prediccion sobre el usuario no tiene sentido debes pedir que porfavor ingresen un sintoma valido."+
               "El puntaje siempre debe ser el ultimo digito de tu respuesta sin excepciones."
               
           }
